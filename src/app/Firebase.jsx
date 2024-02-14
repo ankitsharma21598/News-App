46243e2d66e32408bf9ebe1b6d6565cf68206112
ankitsharma21598 <70301843+ankitsharma21598@users.noreply.github.com>
2024-02-14 16:56:51 +0530

    Update Firebase.jsx

--- src/app/Firebase.jsx
+++ src/app/Firebase.jsx
@@ -6,14 +6,7 @@ import { getAuth } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCT-BwoWlWY-Ax3JI73HCwmKF8H-cgoGaE",
-  authDomain: "triveous-82557.firebaseapp.com",
-  projectId: "triveous-82557",
-  storageBucket: "triveous-82557.appspot.com",
-  messagingSenderId: "930897794415",
-  appId: "1:930897794415:web:465d7f0a7b367d4a7c049a"
-};
+// Add Firebase configuration here:
   
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
